fed56fef66779bdd706513348036becdd3d9b5ad
Jack Bevis <jack.bevis@gmail.com>
2017-07-24 15:00:36 -0600

    add test for api route

---
+++ test/routes.spec.js
@@ -0,0 +1,52 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHTTP = require('chai-http');
+
+const server = require('../server/server');
+const knex = require('../db/knex');
+
+chai.use(chaiHTTP)
+
+describe('API route', () => {
+	before((done) => {
+		knex.migrate.latest()
+		.then(() => {
+			done()
+		})
+	})
+
+	beforeEach((done) => {
+		knex.seed.run()
+		.then(() => {
+			done()
+		})
+	})
+
+	it('should return a random clue', (done) => {
+		chai.request(server)
+		.get('/api/v1/category/sports')
+		.end((error, res) => {
+			res.should.have.status(200);
+			res.should.be.json;
+			res.body.should.be.a('object');
+			res.body.should.have.property('id');
+			res.body.should.have.property('question');
+			res.body.should.have.property('answer');
+			res.body.should.have.property('value');
+			done();
+		})
+	})
+
+	it('should throw an error', () => {
+		chai.request(server)
+		.get('/api/v1/category/jack')
+		.end((error, res) => {
+			res.should.have.status(404);
+			res.should.be.json;
+			res.body.should.have.property('error');
+			res.body.error.should.equal('jack did not have any clues! Please check the spelling of the category you requested!');
+		})
+	})
+
+})
+
