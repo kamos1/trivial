4d5fee78685e3d619cb9d28239368cc8d9043c42
kamos1 <keji.amos@gmail.com>
2017-07-24 16:28:19 -0600

    update knexfile config

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/trivial_test' || process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL || 'postgres://localhost/trivial_test',
     migrations: {
       directory: './db/migrations'
     },
