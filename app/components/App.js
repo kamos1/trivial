01df33ed9f23d57ec3c279d35a9993a5ae4451b0
Kelly Miller <kellyannemiller@gmail.com>
2017-07-25 10:41:31 -0600

    start componenets and sockets

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Route } from 'react-router-dom';
+
 
 export default class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+  }
 
+  componentWillMount(){
   }
 
   render(){
