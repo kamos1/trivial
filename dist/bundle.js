658a428a40051b2b38d938d619034d56679d606b
kamos1 <keji.amos@gmail.com>
2017-07-31 10:22:59 -0600

    rebuild bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "3d1260daa5b043ba83be"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "1229f4f1bf268faf1f5e"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
